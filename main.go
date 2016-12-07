548f4741c1d7e375ac460579691d0820ceeb76f0
Yevhenii Havrylko <egavrilko@gmail.com>
2016-12-07 12:37:29 +0200

    second

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"flag"
 	"fmt"
+	"strings"
 
 	"log"
 
@@ -17,7 +18,7 @@ var (
 )
 
 func main() {
-	db, err := sql.Open("mysql", "root@tcp(192.168.2.84:3306)/rtb")
+	db, err := sql.Open("mysql", "root@tcp(localhost:3306)/rtb")
 	if err != nil {
 		panic(err.Error())
 	}
@@ -29,15 +30,55 @@ func main() {
 	}
 	defer stmtIns.Close()
 
-	//_, err = stmtIns.Exec("http://c8.net.ua/", nil, "C8 SSP", "C8 DSP")
-	//
-	//if err != nil {
-	//	panic(err.Error())
-	//}
+	getTables, err := db.Prepare("show tables;")
+	if err !=nil{
+		panic(err.Error())
+	}
+	defer getTables.Close()
+
+
+	
+
+	//stmtSel, err := db.Prepare("SELECT * FROM ")
+
+	// _, err = stmtIns.Exec("http://c8.net.ua/", nil, "C8 SSP", "C8 DSP")
+	
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
 
 	flag.Parse()
 
-	h := requestHandler
+	h := func(ctx *fasthttp.RequestCtx) {
+		path:= string(ctx.Path()) 
+		if strings.HasPrefix(path,"/table/"){
+			fmt.Fprintf(ctx, "Request method is %q\n", ctx.Method())
+			fmt.Fprintf(ctx, "RequestURI is %q\n", ctx.RequestURI())
+			fmt.Fprintf(ctx, "Requested path is %q\n", ctx.Path())
+			fmt.Fprintf(ctx, "Host is %q\n", ctx.Host())
+			fmt.Fprintf(ctx, "Query string is %q\n", ctx.QueryArgs())
+			fmt.Fprintf(ctx, "User-Agent is %q\n", ctx.UserAgent())
+			fmt.Fprintf(ctx, "Connection has been established at %s\n", ctx.ConnTime())
+			fmt.Fprintf(ctx, "Request has been started at %s\n", ctx.Time())
+			fmt.Fprintf(ctx, "Serial request number for the current connection is %d\n", ctx.ConnRequestNum())
+			fmt.Fprintf(ctx, "Your ip is %q\n\n", ctx.RemoteIP())
+			
+			fmt.Fprintf(ctx, "Raw request is:\n---CUT---\n%s\n---CUT---", &ctx.Request)
+
+			ctx.SetContentType("text/plain; charset=UTF-8")	
+		} else{
+			var table string
+			tables, err := getTables.Query()
+			if err!=nil{
+				panic(err.Error())
+			}
+			for tables.Next(){
+				tables.Scan(&table)
+				fmt.Fprintf(ctx, "<p><a href='/table/"+table+"'>"+table+"</a></p>")
+			}
+			ctx.SetContentType("text/html; charset=UTF-8")
+		}
+	}
 	if *compress {
 		h = fasthttp.CompressHandler(h)
 	}
@@ -45,34 +86,4 @@ func main() {
 	if err := fasthttp.ListenAndServe(*addr, h); err != nil {
 		log.Fatalf("Error in ListenAndServe: %s", err)
 	}
-}
-
-func requestHandler(ctx *fasthttp.RequestCtx) {
-	fmt.Fprintf(ctx, "Hello, world!\n\n")
-
-	fmt.Fprintf(ctx, "<form><input type='text' /><input type='submit' /></form>")
-
-	//fmt.Fprintf(ctx, "Request method is %q\n", ctx.Method())
-	//fmt.Fprintf(ctx, "RequestURI is %q\n", ctx.RequestURI())
-	//fmt.Fprintf(ctx, "Requested path is %q\n", ctx.Path())
-	//fmt.Fprintf(ctx, "Host is %q\n", ctx.Host())
-	//fmt.Fprintf(ctx, "Query string is %q\n", ctx.QueryArgs())
-	//fmt.Fprintf(ctx, "User-Agent is %q\n", ctx.UserAgent())
-	//fmt.Fprintf(ctx, "Connection has been established at %s\n", ctx.ConnTime())
-	//fmt.Fprintf(ctx, "Request has been started at %s\n", ctx.Time())
-	//fmt.Fprintf(ctx, "Serial request number for the current connection is %d\n", ctx.ConnRequestNum())
-	//fmt.Fprintf(ctx, "Your ip is %q\n\n", ctx.RemoteIP())
-	//
-	//fmt.Fprintf(ctx, "Raw request is:\n---CUT---\n%s\n---CUT---", &ctx.Request)
-
-	ctx.SetContentType("text/html; charset=UTF-8")
-
-	// Set arbitrary headers
-	//ctx.Response.Header.Set("content-type", "text/html; charset=UTF-8")
-
-	// Set cookies
-	//var c fasthttp.Cookie
-	//c.SetKey("cookie-name")
-	//c.SetValue("cookie-value")
-	//ctx.Response.Header.SetCookie(&c)
 }
